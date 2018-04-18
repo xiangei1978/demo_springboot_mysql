d2c8cdfb66c22a8f531e94883a40427cf99aa2eb
xianglei <xianglei@MacBook-Pro-davidxl.local>
2018-04-18 16:51:45 +0800

    Initial commit

---
+++ src/main/java/com/davidxl/DemoSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.davidxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootApplication.class, args);
+	}
+}
